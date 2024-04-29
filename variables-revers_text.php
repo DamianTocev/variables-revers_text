f72e4f4898b8677e56971c87e8d53a52bbaaafc2
DamianTocev <84261493+DamianTocev@users.noreply.github.com>
2024-04-29 12:39:48 +0200

    Add files via upload

---
+++ variables-revers_text.php
@@ -0,0 +1,7 @@
+<?php
+$text = ".esoprup no semitemos ,tnedicca yb semitemos ,sraey eht revo devlove evah snoisrev suoiraV .ycnafni rieht ni llits setis bew ynam revocnu lliw 'muspi merol' rof hcraes a dna ,txet ledom tluafed rieht sa muspI meroL esu won srotide egap bew dna segakcap gnihsilbup potksed ynaM .hsilgnE elbadaer ekil kool ti gnikam ,'ereh tnetnoc ,ereh tnetnoC' gnisu ot desoppo sa ,srettel fo noitubirtsid lamron ssel-ro-erom a sah ti taht si muspI meroL gnisu fo tniop ehT .tuoyal sti ta gnikool nehw egap a fo tnetnoc elbadaer eht yb detcartsid eb lliw redaer a taht tcaf dehsilbatse gnol a si tI";
+
+$reversed_text = strrev($text);
+
+echo "<p>" . $reversed_text . "</p>";
+?>
